4ecd0d2ee3d239196a2cf661411aba23c854be9e
MorseAmalgam <HgDelirium@gmail.com>
2025-03-20 15:48:53 -0700

    Updates for the default Chat Trigger
    Numerous changes to the ChatTrigger class. Important ones of note:
    - where_to_match has been given an "exact match" option (E.G. prevent matching "catastrophe" for "cat")
    - Case sensitive flag
    - matches_alias now returns an integer of the number of matches it got, rather than true/false on whether it got a match.

--- addons/twinge/triggers/chat_trigger.gd
+++ addons/twinge/triggers/chat_trigger.gd
@@ -1,20 +1,33 @@
 extends TwingeTriggerTemplate
 class_name ChatTrigger
 
-@export_enum("At Start", "Anywhere") var where_to_match = 0
+const emote_suffixes = ["_BW", "_HF", "_SG", "_SQ", "_TK"]
+@export var case_sensitive:bool = false
+@export_enum("At Start", "Anywhere", "Anywhere (Exact)") var where_to_match = 0
 @export var aliases:String = ""
 var aliases_array:PackedStringArray
 
 func _ready() -> void:
 	aliases_array = aliases.split(",")
 	for i in range(aliases_array.size()):
-		aliases_array[i] = aliases_array[i].strip_edges()
+		if !case_sensitive:
+			aliases_array[i] = aliases_array[i].strip_edges().to_lower()
+		else:
+			aliases_array[i] = aliases_array[i].strip_edges()
 	pass
 
-func matches_alias(message)->bool:
+func matches_alias(message:String)->int:
+	if !case_sensitive:
+		message = message.to_lower()
 	var words = message.split(" ")
+	if (where_to_match == 0 and aliases_array.has(words[0])):
+		return 1
 	for alias in aliases_array:
-		if ((where_to_match == 0 and words[0] == alias) or
-			(where_to_match == 1 and message.contains(alias))):
-				return true
-	return false
+		if (where_to_match == 1):
+			return message.count(alias)
+		elif (where_to_match == 2):
+			var regex = RegEx.new()
+			regex.compile("\\W(%s)\\W" % alias)
+			var results = regex.search_all(message)
+			return results.size()
+	return 0
