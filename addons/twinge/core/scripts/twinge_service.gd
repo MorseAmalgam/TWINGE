82d2645fb1bbaea13bdbdf79421bfd5e04c7dd50
MorseAmalgam <HgDelirium@gmail.com>
2025-03-20 15:42:19 -0700

    Output debug controls!
    Added a system to filter debug message levels.

--- addons/twinge/core/scripts/twinge_service.gd
+++ addons/twinge/core/scripts/twinge_service.gd
@@ -2,16 +2,16 @@ extends Node
 class_name TwingeService
 
 enum DebugType {MESSAGE, WARNING, ERROR}
+@export_enum("None", "Errors", "Warnings & Errors", "Everything") var debug_level = 2
 
 @onready var twinge:TwingeCore = get_parent()
 const utilities = preload("twinge_utilities.gd")
 var service_identifier:String = "Service"
 
 func debug_message(message:String, type:DebugType=DebugType.MESSAGE):
-	match(type):
-		DebugType.MESSAGE:
-			print("[TWINGE-%s] %s" % [service_identifier, message])
-		DebugType.WARNING:
-			push_warning("[TWINGE-%s] %s" % [service_identifier, message])
-		DebugType.ERROR:
-			push_error("[TWINGE-%s] %s" % [service_identifier, message])
+	if (type == DebugType.MESSAGE and debug_level == 3):
+		print("[TWINGE-%s] %s" % [service_identifier, message])
+	elif (type == DebugType.WARNING and debug_level >= 2):
+		push_warning("[TWINGE-%s] %s" % [service_identifier, message])
+	elif (type == DebugType.ERROR and debug_level >= 1):
+		push_error("[TWINGE-%s] %s" % [service_identifier, message])
