6bbd4c251d7c25b4717e452fddc4b5a2184e527f
MorseAmalgam <HgDelirium@gmail.com>
2025-03-22 00:06:17 -0700

    arbitrary changing of parameter names
    No really, I just changed it because these aren't commands, they're redeems

--- addons/twinge/redeems/generic/sound_single.gd
+++ addons/twinge/redeems/generic/sound_single.gd
@@ -2,7 +2,7 @@ extends TwingeSoundRedeemTemplate
 
 @export var sound:AudioStream
 
-func run(_command_user, _details)->bool:
+func run(_user, _details)->bool:
 	player.stream = sound
 	player.play()
 	return true
